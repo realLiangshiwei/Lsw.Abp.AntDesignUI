b3abecea6d9169e32ddf3fad28138842fece2fda
liangshiwei <deawback@hotmail.com>
2023-12-03 14:44:36 +0800

    ABP and NET 8.0

--- samples/BookStore/src/BookStore.EntityFrameworkCore/Migrations/BookStoreDbContextModelSnapshot.cs
+++ samples/BookStore/src/BookStore.EntityFrameworkCore/Migrations/BookStoreDbContextModelSnapshot.cs
@@ -19,7 +19,7 @@ namespace BookStore.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("_Abp_DatabaseProvider", EfCoreDatabaseProvider.SqlServer)
-                .HasAnnotation("ProductVersion", "7.0.10")
+                .HasAnnotation("ProductVersion", "8.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
